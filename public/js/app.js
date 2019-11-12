9e79d26c3e1983826015dfbcf426edd42564e2b2
gdm-1718-brundeco <32094744+gdm-1718-brundeco@users.noreply.github.com>
2019-11-13 00:14:54 +0100

    final commit

--- public/js/app.js
+++ public/js/app.js
@@ -3,32 +3,67 @@ const secondRow = document.getElementById('row2')
 const thirdRow = document.getElementById('row3')
 const db = firebase.firestore();
 
-db.collection('devices').get().then((devices) => {
-    devices.forEach((doc) => {
-        let devType = doc.data().device_name
-        let cta = document.createElement('div')
-        cta.className = 'cta ' + doc.data().led_color
-        if (devType === 'light') {
-            cta.innerHTML = doc.data().device_position
-            cta.id = doc.id
-            cta.addEventListener('click', () => toggleStatus(doc.id, doc.data().status))
-            row1.appendChild(cta)
-        } else if (devType === 'socket') {
-            cta.innerHTML = doc.data().device_position
-            cta.id = doc.id
-            cta.addEventListener('click', () => toggleStatus(doc.id, doc.data().status))
-            row2.appendChild(cta)
-        } else if (devType === 'door') {
-            cta.innerHTML = doc.data().device_position
-            cta.id = doc.id
-            cta.addEventListener('click', () => toggleStatus(doc.id, doc.data().status))
-            row3.appendChild(cta)
-        }
-    })
-});
+renderDevices = () => {
+    db.collection('devices').get().then((devices) => {
+        removeChildEls()
+        devices.forEach((doc) => {
+            let devType = doc.data().device_name
+            let cta = document.createElement('div')
+            let statusIndicator = document.createElement('div')
+            cta.className = 'cta ' + doc.data().led_color
+            if (devType === 'light') {
+                cta.innerHTML = doc.data().device_position
+                cta.id = doc.id
+                cta.addEventListener('click', () => toggleStatus(doc.id, doc.data().status))
+                statusIndicator.classList.add(doc.data().status ? 'active' : 'non-active')
+                cta.appendChild(statusIndicator)
+                row1.appendChild(cta)
+            } else if (devType === 'socket') {
+                cta.innerHTML = doc.data().device_position
+                cta.id = doc.id
+                cta.addEventListener('click', () => toggleStatus(doc.id, doc.data().status))
+                statusIndicator.classList.add(doc.data().status ? 'active' : 'non-active')
+                cta.appendChild(statusIndicator)
+                row2.appendChild(cta)
+            } else if (devType === 'door') {
+                cta.innerHTML = doc.data().device_position
+                cta.id = doc.id
+                cta.addEventListener('click', () => toggleStatus(doc.id, doc.data().status))
+                statusIndicator.classList.add(doc.data().status ? 'active' : 'non-active')
+                cta.appendChild(statusIndicator)
+                row3.appendChild(cta)
+            }
+        })
+    });
+}
+
+removeChildEls = () => {
+    firstRow.innerHTML = ''
+    secondRow.innerHTML = ''
+    thirdRow.innerHTML = ''
+}
+renderDevices()
 
 toggleStatus = (id, status) => {
     db.collection('devices').doc(id).update({
         status: !status
     })
+    renderDevices()
 }
+
+fetchSensorData = () => {
+    db.collection('sensordata').get().then((sensordata) => {
+        sensordata.forEach((doc) => {
+            let temperature = (doc.data().temperature).toFixed(2) + ' °C'
+            let humidity = doc.data().humidity.toFixed(2) + ' %'
+            let childDivTemp = document.querySelector('.child-div.temp')
+            let childDivHum = document.querySelector('.child-div.hum')
+
+            childDivTemp.innerHTML = temperature;
+            childDivHum.innerHTML = humidity
+        })
+    });
+}
+fetchSensorData()
+
+
--- public/pi/app_domotica.py
+++ public/pi/app_domotica.py
@@ -3,27 +3,57 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from sense_hat import SenseHat
 sense = SenseHat()
+from time import sleep
 
 colorBlue = [30, 30, 255]
 colorYellow = [255, 255, 30]
 colorRed = [255, 30, 30]
+
+colorDBlue = [0, 0, 50]
+colorDYellow = [50, 50, 0]
+colorDRed = [50, 0, 0]
 
 # Use a service account
 cred = credentials.Certificate('private_key/pk_domotica.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
 docs = db.collection(u'devices').get()
 
-for doc in docs:
-    dd = doc.to_dict()
-    if dd['status']:
-        if dd['device_name'] == 'socket':
-            sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorBlue)
-        elif dd['device_name'] == 'door':
-            grid_pos = dd['matrix_position']
-            sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorRed)
-        elif dd['device_name'] == 'light':
-            grid_pos = dd['matrix_position']
-            sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorYellow)
+
+def on_snapshot(doc_snapshot, changes, read_time):    
+    for doc in docs:
+        dd = doc.to_dict()
+        print(dd['status'])
+        if dd['status']:
+            if dd['device_name'] == 'socket':
+                sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorBlue)
+            elif dd['device_name'] == 'door':
+                grid_pos = dd['matrix_position']
+                sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorRed)
+            elif dd['device_name'] == 'light':
+                grid_pos = dd['matrix_position']
+                sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorYellow)
+        else:
+            if dd['device_name'] == 'socket':
+                sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorDBlue)
+            elif dd['device_name'] == 'door':
+                grid_pos = dd['matrix_position']
+                sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorDRed)
+            elif dd['device_name'] == 'light':
+                grid_pos = dd['matrix_position']
+                sense.set_pixel(dd['matrix_position'][0], dd['matrix_position'][1], colorDYellow)
+                
+    sensor_values = {
+        'humidity': sense.get_humidity(),
+        'temperature': sense.get_temperature()
+    }
+    
+    db.collection('sensordata').document('sensordata').set(sensor_values)
+           
+doc_ref = db.collection(u'devices')
+doc_watch = doc_ref.on_snapshot(on_snapshot)
+
+
+while True:
+    sleep(3)
